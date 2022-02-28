cd538e36e026f21a0e9dc11efe359b39c85c30f6
forrestmeng <33429166+forrestmeng@users.noreply.github.com>
2022-02-28 13:09:00 -0500

    Update barchart.js

--- js/barchart.js
+++ js/barchart.js
@@ -9,6 +9,7 @@ Modified: 12/08/21
 // Build your bar charts in this file 
 
 
+
 // Set dimensions and margins for plots 
 const width = 900; 
 const height = 450; 
@@ -17,12 +18,16 @@ const yTooltipOffset = 15;
 
 
 // TODO: What does this code do? 
-const svg1 = d3
-  .select("#hard-coded-bar")
-  .append("svg")
+// adding svg to hard coded bar and sets width, height, and viewbox
+const svg1 = d3.select("#hard-coded-bar").append("svg")
   .attr("width", width-margin.left-margin.right)
   .attr("height", height - margin.top - margin.bottom)
   .attr("viewBox", [0, 0, width, height]);
+
+
+//const data1 = d3.csv("barchart.csv", function(data){
+//    console.log(data)
+//});
 
 // Hardcoded barchart data
 const data1 = [
@@ -42,26 +47,31 @@ const data1 = [
 */ 
 
 // TODO: What does this code do? 
+// looks at data1 and pulls max
 let maxY1 = d3.max(data1, function(d) { return d.score; });
 
 // TODO: What does each line of this code do?   
+// sets a scaling function for the y axis for data values to pixel values
 let yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
             .range([height-margin.bottom,margin.top]); 
 
 // TODO: What does each line of this code do? 
+// sets a scaling function for the x axis for data values to pixel values, adds padding
 let xScale1 = d3.scaleBand()
             .domain(d3.range(data1.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
 
 // TODO: What does each line of this code do?  
+// takes svg1, append a generic svg to it, and makes the axis for us and make the font larger
 svg1.append("g")
    .attr("transform", `translate(${margin.left}, 0)`) 
    .call(d3.axisLeft(yScale1)) 
    .attr("font-size", '20px'); 
 
 // TODO: What does each line of this code do? 
+// takes svg1, append a generic svg to it, translate to the spot we want it, use axisBottom to add x-axis, also use tickFormat to set the name at each tick (whatever row we are on, and pull name)
 svg1.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`) 
     .call(d3.axisBottom(xScale1) 
@@ -75,25 +85,29 @@ svg1.append("g")
 */
 
 // TODO: What does each line of this code do? 
+// selecting hard coded bar and appending div to it with ID tooltip 1, with 0 opacity and class "toooltip"
 const tooltip1 = d3.select("#hard-coded-bar") 
                 .append("div") 
                 .attr('id', "tooltip1") 
                 .style("opacity", 0) 
                 .attr("class", "tooltip"); 
 
-// TODO: What does each line of this code do?  
+// TODO: What does each line of this code do? 
+// uses tooltip we just created and display information when we hover over, then sets opacity to 1 so we can see it
 const mouseover1 = function(event, d) {
   tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
           .style("opacity", 1);  
 }
 
 // TODO: What does each line of this code do? 
+// positions the tooltip a bit offset from the mouse, moves with the mouse
 const mousemove1 = function(event, d) {
   tooltip1.style("left", (event.x)+"px") 
           .style("top", (event.y + yTooltipOffset) +"px"); 
 }
 
 // TODO: What does this code do? 
+// when the mouse leaves the bar, the opacity of the tooltip is 0
 const mouseleave1 = function(event, d) { 
   tooltip1.style("opacity", 0); 
 }
@@ -105,6 +119,7 @@ const mouseleave1 = function(event, d) {
 */
 
 // TODO: What does each line of this code do? 
+// look at svg1 and select everything from the class bar, empty selection, appends a rectangle to svg1 for each placeholder(row), adds attribute class - make it bar, scales x position of a rectangle, setting y position of each of the rectagles as the y position of each score map, set height and width of each bar, bandwith automatically sets width of bars based on how many there are, add event listener to the bars and calls actions to handle events when mouse is moved
 svg1.selectAll(".bar") 
    .data(data1) 
    .enter()  
